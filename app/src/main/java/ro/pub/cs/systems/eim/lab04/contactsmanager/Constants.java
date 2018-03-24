2ceb20f2fa5a916aba54645c380a1855b707098a
iulia-ni <iulia.nicula@stud.acs.upb.ro>
2018-03-24 22:31:59 +0200

    removed app

--- app/src/main/java/ro/pub/cs/systems/eim/lab04/contactsmanager/Constants.java
+++
@@ -1,9 +0,0 @@
-package ro.pub.cs.systems.eim.lab04.contactsmanager;
-
-/**
- * Created by root on 24.03.2018.
- */
-
-class Constants {
-    final static int CONTACTS_MANAGER_REQUEST_CODE = 2018;
-}
